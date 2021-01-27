ba751176ec8cb4e34d02cd03762ad4fff83f6318
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-27 00:00:10 +0000

    added tweet length counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("textarea#tweet-text").on("input", function() {
+    let numberOfChars = $(this).val().length;
+    $(this).siblings().children(".counter").html(140 - numberOfChars)
+    if ($(this).siblings().children(".counter").html() < 0) {
+      $(this).siblings().children(".counter").css("color", "red");
+    }
+  })
+});
