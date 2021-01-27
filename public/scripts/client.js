ea321e168e7a5100061be4360163f2f78abd5565
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-27 21:27:13 +0000

    added renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,37 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+        "content": {
+          "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+      }
+    ]
+    const renderTweets = function(tweets) {
+      for (const entry of tweets) {
+        console.log(entry);
+        const $tweet = createTweetElement(entry)
+        $('section.tweets').append($tweet); 
+      }
+    }
   const createTweetElement = function(tweetObject) {
     const avatar = tweetObject.user.avatars;
     const text = tweetObject.content.text;
@@ -26,22 +57,6 @@ $(document).ready(function() {
     return markup;
   }
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
   
-  const $tweet = createTweetElement(tweetData);
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('section.tweets').append($tweet); // to add it to the page s
-
-  
+  renderTweets(data);
 })
