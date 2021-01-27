7b65fa10cc4a0609dbcd2b6981041d8ea3894df6
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-27 21:04:03 +0000

    created createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,45 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const createTweetElement = function(tweetObject) {
+    const avatar = tweetObject.user.avatars;
+    const text = tweetObject.content.text;
+    const handle = tweetObject.user.handle;
+    const name = tweetObject.user.name;
+    const timeAgo = tweetObject.created_at;
+    const markup = `
+    <div class="tweets">
+      <a class = "tweets">
+        <header class="tweets">
+          <div class="user"><img class="tweets avatar" src= ${avatar}>${name}</div>
+          <p class="handle">${handle}</p>
+        </header>
+        <p class="tweets">${text}</p>
+        <footer class="tweets">
+          <p>${timeAgo}</p><div><img class="icon" src="images/flag.svg"><img class="icon" src="images/repeat.svg"><img class="icon" src="images/heart.svg"></div>
+        </footer>
+      </a>
+    </div>`;
+    return markup;
+  }
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+  
+  const $tweet = createTweetElement(tweetData);
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('section.tweets').append($tweet); // to add it to the page s
 
+  
+})
