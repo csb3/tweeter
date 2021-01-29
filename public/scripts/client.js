cf0ba99921ec71fc4700a8f33f55a34482bda4f8
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-29 00:07:51 +0000

    linted/formatted code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /*
 * Client-side JS logic goes here
 * jQuery is already loaded
@@ -6,61 +7,61 @@
 
 
 $(document).ready(function() {
- const loadTweets = function() {
-   $.ajax('/tweets', { method: 'GET' })
-   .then(function(data){
-     renderTweets(data);
-     $("time.timeago").timeago();
-   });
-   };
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(data) {
+        renderTweets(data);
+        $("time.timeago").timeago();
+      });
+  };
 
- loadTweets();
+  loadTweets();
 
- const renderTweets = function(tweets) {
-   for (const entry of tweets) {
-     const $tweet = createTweetElement(entry)
-     $('section.tweets').append($tweet); 
-   }
- }
- const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
- const createTweetElement = function(tweetObject) {
-   const avatar = tweetObject.user.avatars;
-   const text = escape(tweetObject.content.text);
-   const handle = tweetObject.user.handle;
-   const name = tweetObject.user.name;
-   const date = new Date(tweetObject.created_at).toISOString();
-   const markup = `
+  const renderTweets = function(tweets) {
+    for (const entry of tweets) {
+      const $tweet = createTweetElement(entry);
+      $('section.tweets').append($tweet);
+    }
+  };
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  const createTweetElement = function(tweetObject) {
+    const avatar = tweetObject.user.avatars;
+    const text = escape(tweetObject.content.text);
+    const handle = tweetObject.user.handle;
+    const name = tweetObject.user.name;
+    const date = new Date(tweetObject.created_at).toISOString();
+    const markup = `
    <div class="tweets">
-   <a class = "tweets">
-   <header class="tweets">
-   <div class="user"><img class="tweets avatar" src= ${avatar}>${name}</div>
-   <p class="handle">${handle}</p>
-   </header>
-   <body>
-   <p class="tweets">${text}</p></body>
-   <footer class="tweets">
-   <p><time class="timeago" datetime="${date}">${date}</time></p><div><img class="icon" src="images/flag.svg"><img class="icon" src="images/repeat.svg"><img class="icon" src="images/heart.svg"></div>
-   </footer>
-   </a>
+    <a class = "tweets">
+      <header class="tweets">
+        <div class="user"><img class="tweets avatar" src= ${avatar}>${name}</div>
+        <p class="handle">${handle}</p>
+      </header>
+      <p class="tweets">${text}</p>
+      <footer class="tweets">
+        <p><time class="timeago" datetime="${date}">${date}</time></p>
+        <div><img class="icon" src="images/flag.svg"><img class="icon" src="images/repeat.svg"><img class="icon" src="images/heart.svg"></div>
+      </footer>
+    </a>
    </div>`;
-   return markup;
- }
+    return markup;
+  };
   
   $('#tweet-form').on("submit", function(event) {
     event.preventDefault();
     console.log($('#tweet-text').val().length);
-    if($('#tweet-text').val().length === 0 || $('#tweet-text').val() === null) {
+    if ($('#tweet-text').val().length === 0 || $('#tweet-text').val() === null) {
       $('a.error').html("! Tweet cannot be blank");
-      $("a.error").slideDown( "fast", function() {
+      $("a.error").slideDown("fast", function() {
         // Animation complete.
       });
     } else if ($('#tweet-text').val().length > 140) {
       $('a.error').html("! Tweet cannot exceed 140 characters");
-      $("a.error").slideDown( "fast", function() {
+      $("a.error").slideDown("fast", function() {
         // Animation complete.
       });
     } else {
@@ -70,14 +71,14 @@ $(document).ready(function() {
         data: $('#tweet-text').serialize(),
       }).then(() => {
         $('section.tweets').empty();
-        loadTweets()
-        $('#tweet-text').val('')
-        $("a.error").slideUp( "fast", function() {
+        loadTweets();
+        $('#tweet-text').val('');
+        $("a.error").slideUp("fast", function() {
           // Animation complete.
         });
         $('a.error').html("");
         $(".counter").html(140);
-      })
+      });
     }
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-undef */
 $(document).ready(function() {
   $("textarea#tweet-text").on("input", function() {
     let numberOfChars = $(this).val().length;
-    $(this).siblings().children(".counter").html(140 - numberOfChars)
+    $(this).siblings().children(".counter").html(140 - numberOfChars);
     if ($(this).siblings().children(".counter").html() < 0) {
       $(this).siblings().children(".counter").css("color", "red");
     } else {
       $(this).siblings().children(".counter").css("color", "#545149");
     }
-  })
+  });
 });
